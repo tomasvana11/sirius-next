9646d66b1406e5c217346e6d9800883bbccbd19c
tomas-avette <156674165+tomas-avette@users.noreply.github.com>
2025-10-10 01:42:11 +0200

    image test

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,7 @@ const nextConfig: NextConfig = {
 export default nextConfig;
 */
 
+/*
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -17,6 +18,32 @@ const nextConfig: NextConfig = {
         hostname: 'sirius-strapi-qbx63.ondigitalocean.app',
         port: '',
         pathname: '/uploads/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
+*/
+
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      // Obrázky ze Strapi (kdyby někdy byly hostované přímo tam)
+      {
+        protocol: "https",
+        hostname: "sirius-strapi-qbx63.ondigitalocean.app",
+        port: "",
+        pathname: "/uploads/**",
+      },
+      // Obrázky z DigitalOcean Spaces CDN
+      {
+        protocol: "https",
+        hostname: "sirius-file-storage.fra1.cdn.digitaloceanspaces.com",
+        port: "",
+        pathname: "/sirius-file-storage/**",
       },
     ],
   },
